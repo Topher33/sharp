4288528b98507a1b70fd28f021d3b93079361de3
Matt Warren <Matthew_Warren@homedepot.com>
2017-04-17 11:04:47 -0400

    main() with base script

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  bool x = false;
+  int name;
+  name = cin
+
+  if(name == null){
+    cout << "Hello " << name << endl;
+  }
+  return 0;
+}
